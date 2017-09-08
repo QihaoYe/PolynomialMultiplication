53f26f64eae481f85ac30b3766df42b0d96375a6
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-08 19:29:08 +0800

    update testing

--- PolynomialMultiplication.py
+++ PolynomialMultiplication.py
@@ -3,21 +3,20 @@
 __author__ = 'Yee_172'
 __data__ = '2017/8/28'
 
-from Element import *
 from Func import *
 
-x = Indeterminate('x')
-y = Indeterminate('y')
-z = Indeterminate('z')
-alpha1 = Indeterminate('α', subscript='1')
-alpha2 = Indeterminate('α', subscript='2')
-alpha3 = Indeterminate('α', subscript='3')
-beta1 = Indeterminate('β', subscript='1')
-beta2 = Indeterminate('β', subscript='2')
-beta3 = Indeterminate('β', subscript='3')
-gamma1 = Indeterminate('γ', subscript='1')
-gamma2 = Indeterminate('γ', subscript='2')
-gamma3 = Indeterminate('γ', subscript='3')
+# x = Indeterminate('x')
+# y = Indeterminate('y')
+# z = Indeterminate('z')
+# alpha1 = Indeterminate('α', subscript='1')
+# alpha2 = Indeterminate('α', subscript='2')
+# alpha3 = Indeterminate('α', subscript='3')
+# beta1 = Indeterminate('β', subscript='1')
+# beta2 = Indeterminate('β', subscript='2')
+# beta3 = Indeterminate('β', subscript='3')
+# gamma1 = Indeterminate('γ', subscript='1')
+# gamma2 = Indeterminate('γ', subscript='2')
+# gamma3 = Indeterminate('γ', subscript='3')
 
 # Δ = (x^2+y^2+z^2)^(1/2)
 Δcosα = str2polynomial('α_1*x+α_2*y+α_3*z')
@@ -26,5 +25,7 @@ gamma3 = Indeterminate('γ', subscript='3')
 cosαβ = str2polynomial('α_1*β_1+α_2*β_2+α_3*β_3')
 cosαγ = str2polynomial('α_1*γ_1+α_2*γ_2+α_3*γ_3')
 cosβγ = str2polynomial('β_1*γ_1+β_2*γ_2+β_3*γ_3')
-a = Δcosα ** 3
-print(a)
+Δ_2cosα_2 = Δcosα ** 2
+Δ_2cosβ_2 = Δcosβ ** 2
+Δ_2cosγ_2 = Δcosγ ** 2
+print(Δ_2cosα_2)
