699c215187ccb85703f1d7baca1d409f456b1029
Qihao Ye <11610812@mail.sustc.edu.cn>
2017-12-19 09:59:29 +0800

    update

--- PolynomialMultiplication.py
+++ PolynomialMultiplication.py
@@ -13,3 +13,15 @@ P3 = str2polynomial('p_3')
 P4 = str2polynomial('p_1*p_4+p_2*p_5-p_1*p_2*p_4*p_5')
 P5 = str2polynomial('1-p_3')
 print((P1*P2*P3+P4*P5).latex())
+
+
+x0 = str2polynomial('x')
+x1 = str2polynomial('x+1')
+x2 = str2polynomial('x+2')
+x3 = str2polynomial('x+3')
+x4 = str2polynomial('x+4')
+x = [x0, x1, x2, x3, x4]
+
+for i in range(5):
+    for j in range(i, 5):
+        print('x%d * x%d = %s' % (i, j, x[i] * x[j]))
